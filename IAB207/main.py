469384149932e21e7d69b684c2130155e65a2f81
Sebastian-Zitny <sebastianzitny@gmail.com>
2025-05-07 13:51:10 +1000

    img working

--- IAB207/main.py
+++ IAB207/main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 
 
+
 @app.route('/')
 def default():
     return render_template('index.html')
@@ -16,6 +17,21 @@ def Home():
 @app.route('/createEvents.html')
 def createEvents():
     return render_template('createEvents.html')
+
+@app.route('/eventDetail.html')
+def eventDetail():
+    return render_template('eventDetail.html')
+
+@app.route('/myBooking.html')
+def myBooking():
+    return render_template('myBooking.html')
+
+@app.route('/checkOut.html')
+def checkOut():
+    return render_template('checkOut.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False)
