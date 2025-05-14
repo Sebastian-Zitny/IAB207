bfc8ff31225e581ab04ba0f3f0a89a36b4bb040a
Sebastian-Zitny <sebastianzitny@gmail.com>
2025-05-14 14:50:30 +1000

    added login

--- IAB207/logIn.py
+++ IAB207/logIn.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
-
+from flask import render_template, request, redirect, url_for, flash, session
+from databaseCreator import db, User
 
 def init_logIn(app):
-    @app.route('/logIn.html')
+    @app.route('/logIn.html', methods=['GET', 'POST'])
     def logIn():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            # Find user by username and password
+            user = User.query.filter_by(username=username, password=password).first()
+
+            if user:
+                # Log the user in
+                session['user_id'] = user.user_id
+                session['username'] = user.username
+                flash("Login successful!", "success")
+                return redirect(url_for('myBooking'))  # or your target route
+            else:
+                flash("Invalid username or password.", "error")
+                return redirect(url_for('logIn'))
+
         return render_template('logIn.html')
-
-
-
